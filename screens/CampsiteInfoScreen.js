ae732ee4e7acb1ead896718437cf084aa396d091
KevGB <kevingbice@gmail.com>
2023-03-28 11:50:50 -0700

    Creates comments list for CampsiteInfoScreen

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,52 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
+import { useState } from "react";
+import { FlatList, Text, View, StyleSheet } from "react-native";
+import { COMMENTS } from "../shared/comments";
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
-  return <RenderCampsite campsite={campsite} />;
+  const [comments, setComments] = useState(COMMENTS);
+
+  const renderCommentItem = ({ item }) => {
+    return (
+      <View style={styles.commentItem}>
+        <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+        <Text style={{ fontSize: 12 }}>{`--${item.author}, ${item.date}`}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList
+      data={comments.filter((comment) => comment.campsiteId === campsite.id)}
+      renderItem={renderCommentItem}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20 }}
+      ListHeaderComponent={
+        <>
+          <RenderCampsite campsite={campsite} />
+          <Text style={styles.commentsTitle}>Comments</Text>
+        </>
+      }
+    />
+  );
 };
+
+const styles = StyleSheet.create({
+  commentsTitle: {
+    textAlign: "center",
+    backgroundColor: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#43484d",
+    padding: 10,
+    paddingTop: 30,
+  },
+  commentItem: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#fff",
+  },
+});
 
 export default CampsiteInfoScreen;
