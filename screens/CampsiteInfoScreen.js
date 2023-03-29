5bc181d160260f4970570ae5da5ca1ba6fd3335c
KevGB <kevingbice@gmail.com>
2023-03-29 09:44:19 -0700

    Using Redux

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,17 +1,17 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
 import { useState } from "react";
 import { FlatList, Text, View, StyleSheet } from "react-native";
-import { COMMENTS } from "../shared/comments";
+import { useSelector } from "react-redux";
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
-  const [comments, setComments] = useState(COMMENTS);
+  const comments = useSelector((state) => state.comments);
   const [favorite, setFavorite] = useState(false);
 
   const renderCommentItem = ({ item }) => {
     return (
       <View style={styles.commentItem}>
-        <Text style={{fontSize: 14}}>{item.text}</Text>
+        <Text style={{ fontSize: 14 }}>{item.text}</Text>
         <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
         <Text style={{ fontSize: 12 }}>{`--${item.author}, ${item.date}`}</Text>
       </View>
@@ -20,7 +20,7 @@ const CampsiteInfoScreen = ({ route }) => {
 
   return (
     <FlatList
-      data={comments.filter((comment) => comment.campsiteId === campsite.id)}
+      data={comments.commentsArray.filter((comment) => comment.campsiteId === campsite.id)}
       renderItem={renderCommentItem}
       keyExtractor={(item) => item.id.toString()}
       contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20 }}
