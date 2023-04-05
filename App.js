56ccc39def724300951b795588605b7512627b6d
KevGB <kevingbice@gmail.com>
2023-04-04 20:30:30 -0700

    Swipe and Delete Favorites

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import Main from "./screens/MainComponent";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import {store} from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import Loading from "./components/LoadingComponent";
 
 export default function App() {
   return (
-    <Provider store={store} >
-      <NavigationContainer>
-        <Main />
-      </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <NavigationContainer>
+          <Main />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
